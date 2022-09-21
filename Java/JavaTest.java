b65c745b4766554c096c72666ac26c36debed013
tangbingxing <m13467746510@163.com>
2022-09-21 22:49:06 +0800

    新建2

--- Java/JavaTest.java
+++ Java/JavaTest.java
@@ -6,4 +6,15 @@ public class JavaTest {
     public static void main(String[] args) {
         System.out.println("Hallow java!");
     }
+
+    class A {
+        B.cry();
+
+        class B {
+            public static void cry() {
+                System.out.println("cry");
+            }
+        }
+    }
+
 }
